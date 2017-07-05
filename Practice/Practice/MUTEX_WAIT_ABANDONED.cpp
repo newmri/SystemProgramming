b7d7294d1197ee2bf5fed8f350b756c1ac9d6753
newmri <newmri@naver.com>
2017-07-05 15:27:09 +0900

    Chapter 14 Thread Synchronization 1

---
+++ Practice/Practice/MUTEX_WAIT_ABANDONED.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <process.h>
+
+LONG gTotalCount = 0;
+HANDLE hMutex;
+
+unsigned int WINAPI IncreaseCountOne(LPVOID lpParam)
+{
+	WaitForSingleObject(hMutex, INFINITE);
+	gTotalCount++;
+
+	return 0;
+}
+
+unsigned int WINAPI IncreaseCountTwo(LPVOID lpParam)
+{
+	DWORD dwWaitResult = 0;
+	dwWaitResult = WaitForSingleObject(hMutex, INFINITE);
+	switch (dwWaitResult) {
+	case WAIT_OBJECT_0:
+		ReleaseMutex(hMutex);
+		break;
+	case WAIT_ABANDONED:
+		_tprintf(_T("WAIT_ABANDONED \n"));
+		break;
+	}
+
+	gTotalCount++;
+
+	ReleaseMutex(hMutex);
+	return 0;
+}
+
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	DWORD dwThreadIDOne;
+	DWORD dwThreadIDTwo;
+
+	HANDLE hThreadOne;
+	HANDLE hThreadTwo;
+
+	hMutex = CreateMutex(NULL, FALSE, NULL);
+
+	if (hMutex == NULL) _tprintf(_T("CreateMutex error: %d\n"), GetLastError());
+
+	// Rude Thread
+	hThreadOne = (HANDLE)_beginthreadex(NULL, 0, IncreaseCountOne, NULL, 0, (unsigned*)&dwThreadIDOne);
+	hThreadTwo = (HANDLE)_beginthreadex(NULL, 0, IncreaseCountTwo, NULL, CREATE_SUSPENDED, (unsigned*)&dwThreadIDTwo);
+
+	Sleep(1000);
+	ResumeThread(hThreadTwo);
+
+	WaitForSingleObject(hThreadTwo, INFINITE);
+	_tprintf(_T("total count: %d \n"), gTotalCount);
+
+	CloseHandle(hThreadOne);
+	CloseHandle(hThreadTwo);
+	CloseHandle(hMutex);
+
+	return 0;
+
+}
